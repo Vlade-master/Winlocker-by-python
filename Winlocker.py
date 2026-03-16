fcd34b5e9471aca49883d1a396ed310770f4fc14
LAXD <11vladrusin@gmail.com>
2026-03-16 17:48:47 +0300

    Update Winlocker.py

--- Winlocker.py
+++ Winlocker.py
@@ -1,3 +1,9 @@
+#WARNING: This software/source code is intended strictly for educational purposes, antivirus software testing, and security system behavior analysis within a controlled environment.
+#PROJECT PURPOSE: To demonstrate malware mechanisms or to verify the effectiveness of security solutions (PoC — Proof of Concept).
+#LIMITATION OF LIABILITY: The author of this code assumes no liability for any damage caused by its use, misuse, or modification. You use this software at your own risk.
+#PROHIBITION OF ILLEGAL USE: It is strictly prohibited to use this code for cyberattacks, unauthorized access to third-party systems, or any other actions that violate the law.
+#EXECUTION CONDITIONS: The author strongly recommends running this code only in isolated environments (virtual machines, sandboxes) without access to sensitive or critical data.
+
 import tkinter as tk
 from tkinter import messagebox
 import pygame
